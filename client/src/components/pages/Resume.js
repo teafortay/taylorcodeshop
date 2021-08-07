610569935da3daeaa0b754d9020b4ca6c29968dc
Taylor Shaw <tay@taylorcodeshop.com>
2021-08-06 21:39:30 -0400

    added new resume

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -20,7 +20,7 @@ class Resume extends Component {
       return (
         <>
           <h2> Resume Page</h2>
-          <a href="/resume7-6-21.pdf">click here for resume</a>
+          <a href="/TaylorShaw copy.pdf">click here for resume</a>
           </>
     );
   }
