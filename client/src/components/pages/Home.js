24221f10a0d8de9709a5f1709a9b7118831b9056
Taylor Shaw <tay@taylorcodeshop.com>
2024-07-22 21:02:06 -0400

    addad code shop image

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import "../../utilities.css";
 import "./Home.css";
+import logo from "../../../dist/shophammer.png"
 
 class Home extends Component {
   constructor(props) {
@@ -20,8 +21,9 @@ class Home extends Component {
         
         <h1>Welcome to Taylor Shaw's personal website</h1>
       <h2>This is where I blog about what's happening in my code shop.</h2>
+       
+        <img src={logo} alt="Logo image of a storefront with a large capital T. The T is reminiscent of the Xcode logo." />
         <h2> Please explore!</h2>
-        
       </>
     );
   }
